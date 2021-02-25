b8679a0bc4d2f5dbf03e9514ac7125cf71aff81b
Doug Eisenstein <doug.eisenstein@gmail.com>
2021-02-25 12:42:46 -0500

    lambda

--- {{cookiecutter.adapter_code}}/scripts/unix/lambda_update.sh
+++ {{cookiecutter.adapter_code}}/scripts/unix/lambda_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
+echo "Image sha256: $1"
 
 aws lambda update-function-code \
     --function-name {{cookiecutter.adapter_code}} \
-    --image-uri xxxxxxxxxxxxxx.dkr.ecr.us-east-2.amazonaws.com/{{cookiecutter.adapter_code}}@sha256:c0b0e7097316a15486e901c553232e1b4a5b16656541e09129b4412181a6ff8f
+    --image-uri xxxxxxxxxxxxxx.dkr.ecr.us-east-2.amazonaws.com/{{cookiecutter.adapter_code}}@sha256:$1

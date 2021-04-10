65bbdfb6b7e4dfeceaedd10ef41ca73d3e10b7e9
Doug Eisenstein <doug.eisenstein@gmail.com>
2021-04-10 09:26:21 -0400

    lambda

--- {{cookiecutter.adapter_code}}/scripts/unix/lambda_update.sh
+++ {{cookiecutter.adapter_code}}/scripts/unix/lambda_update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "Image tag (include sha256): $1"
-
 AWS_REGION=${2:-{{cookiecutter.aws_region}}}
 AWS_ACCOUNT_ID=${3:-{{cookiecutter.aws_account_id}}}
+IMAGE_TAG=$(git rev-parse HEAD)
 
 aws lambda update-function-code \
     --function-name {{cookiecutter.adapter_code}} \
-    --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/{{cookiecutter.adapter_code}}@$1
+    --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/{{cookiecutter.adapter_code}}:$IMAGE_TAG

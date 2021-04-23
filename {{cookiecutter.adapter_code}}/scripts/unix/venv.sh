d02093dc87e30d053479634347525faa2b385517
Doug Eisenstein <doug.eisenstein@gmail.com>
2021-04-23 16:19:28 -0400

    update

--- {{cookiecutter.adapter_code}}/scripts/unix/venv.sh
+++ {{cookiecutter.adapter_code}}/scripts/unix/venv.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 deactivate
 rm -rf env
-rm -rf *.egg-info
-rm -rf dist
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 pip install virtualenv
 virtualenv env --python=python3.8
 source env/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install jupyter
